a68bf73ab47c5aaf13b1ca281e5073be6e9fe8de
Rob Moran <github@thegecko.org>
2020-05-04 19:24:26 +0100

    Linux

---
+++ src/linux.ts
@@ -0,0 +1,13 @@
+import * as udev from 'udev';
+
+let devices = udev.list('usb');
+let drives = udev.list('block');
+devices = devices.filter(device => !!device.ID_SERIAL_SHORT);
+drives = drives.filter(drive => !!drive.ID_SERIAL_SHORT);
+
+devices.forEach(device => {
+    const mount = drives.find(drive => drive.ID_SERIAL_SHORT === device.ID_SERIAL_SHORT);
+    if (mount) {
+        console.log(`${device.ID_SERIAL_SHORT}: ${mount.ID_FS_LABEL}`);
+    }
+});
