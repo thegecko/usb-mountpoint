da276dc33578288018811372be3043046775c918
thegecko <rob.moran@arm.com>
2020-05-04 20:23:48 +0100

    Fix building on not Linix

--- src/linux.ts
+++ src/linux.ts
@@ -1,13 +1,22 @@
-import * as udev from 'udev';
+import { platform } from 'os';
+const isLinux = platform() === 'linux';
 
-let devices = udev.list('usb');
-let drives = udev.list('block');
-devices = devices.filter(device => !!device.ID_SERIAL_SHORT);
-drives = drives.filter(drive => !!drive.ID_SERIAL_SHORT);
-
-devices.forEach(device => {
-    const mount = drives.find(drive => drive.ID_SERIAL_SHORT === device.ID_SERIAL_SHORT);
-    if (mount) {
-        console.log(`${device.ID_SERIAL_SHORT}: ${mount.ID_FS_LABEL}`);
+(async () => {
+    if (!isLinux) {
+        return;
     }
-});
+
+    const udev = await import('udev');
+
+    let devices = udev.list('usb');
+    let drives = udev.list('block');
+    devices = devices.filter(device => !!device.ID_SERIAL_SHORT);
+    drives = drives.filter(drive => !!drive.ID_SERIAL_SHORT);
+    
+    devices.forEach(device => {
+        const mount = drives.find(drive => drive.ID_SERIAL_SHORT === device.ID_SERIAL_SHORT);
+        if (mount) {
+            console.log(`${device.ID_SERIAL_SHORT}: ${mount.ID_FS_LABEL}`);
+        }
+    });
+})();
